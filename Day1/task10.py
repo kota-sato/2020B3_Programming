dbf0f0b4f32d94469bc9746ebfe7dc89ba327dff
kota-sato <kota271828@keio.jp>
2020-03-02 15:42:32 +0900

    12問目 リストの重複をなくす

--- Day1/task10.py
+++ Day1/task10.py
@@ -5,18 +5,19 @@ char_list = []
 tmp_str = ''
 bi_str = ''
 num = 2
-
+I = 0;
 for i in range(1, len(args) - 1):
     tmp_list = []
     for j in range(0, num ):
         tmp_list.append(args[i +j])
     word_list.append(tmp_list)
     tmp_str += args[i]
+    I = i
 
 print('word bi-gram:'),
 print(word_list)
+tmp_str += args[I+1]
 
-tmp_str += args[i+1]
 for i in range(0, len(tmp_str) - 1):
     bi_str = ''
     tmp_list = []
