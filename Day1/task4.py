e437e6165976b050cfbed5440657e5a0fb14ff0e
kota-sato <kota271828@keio.jp>
2020-03-03 12:03:26 +0900

    task4改

--- Day1/task4.py
+++ Day1/task4.py
@@ -4,11 +4,13 @@ str2 = ''
 str1 = ''
 
 
-for i in range(0, 8):
+'''for i in range(0, 8):
     if i % 2 == 0:
         str2 += org_str[i:i+1]
     else:
         str1 += org_str[i:i+1]
-
 print(str2)
 print(str1)
+'''
+print(org_str[::2])
+print(org_str[1::2])
