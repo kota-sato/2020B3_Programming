4108134216fa73068345130f56508f144a99966f
kota-sato <kota271828@keio.jp>
2020-03-10 16:01:11 +0900

    多層化

---
+++ Day2/task2-2-4.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+class SingleLayer:
+    def __init__(self, x, w, b):
+        self.x = x
+        self.w = w
+        self.b = b
+        self.y = b
+
+    def forward(self):
+        for (row_w, num_x) in zip(self.w, self.x):
+            for (j, num_w) in enumerate(row_w):
+                self.y[j] = self.y[j] + num_w * num_x
+        return self.y
+#        print(y)
+
+
+class MLP_3Layer():
+    def __init__(self, x):
+        self.x = x
+        self.y = x
+        self.W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+        self.b1 = np.array([0.1, 0.2, 0.3])
+        self.W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+        self.b2 = np.array([0.1, 0.2])
+        self.W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+        self.b3 = np.array([0.1, 0.2])
+
+    def forward(self):
+        # for (x, w1, b1) in zip(self.x, self.W1, self.b1):
+        for (i, x) in enumerate(self.x):
+
+            ray1 = SingleLayer(x, self.W1, self.b1)
+            ray2 = SingleLayer(ray1.forward(), self.W2, self.b2)
+            self.b1 = np.array([0.1, 0.2, 0.3])
+            self.b2 = np.array([0.1, 0.2])
+            self.b3 = np.array([0.1, 0.2])
+            ray3 = SingleLayer(ray2.forward(), self.W3, self.b3)
+            self.b1 = np.array([0.1, 0.2, 0.3])
+            self.b2 = np.array([0.1, 0.2])
+            self.b3 = np.array([0.1, 0.2])
+            self.y[i] = ray3.forward()
+        print(self.y)
+
+
+x = np.array([[1.0, 0.5], [-0.3, -0.2], [0.0, 0.8], [0.3, -0.4]])
+
+layers = MLP_3Layer(x)
+layers.forward()
+
