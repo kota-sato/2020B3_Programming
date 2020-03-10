75a91a0372399c28481d68881d0f6b17eb51b07f
kota-sato <kota271828@keio.jp>
2020-03-10 17:20:45 +0900

    逆伝播部分わからず。

---
+++ Day2/task2-3-1.py
@@ -0,0 +1,43 @@
+class Multiply():
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        z = x * y
+        return z
+
+    def backprop(self, dz):
+        dx = dz * self.y
+        dy = dz * self.x
+        return dx, dy
+
+
+class Add():
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        z = x + y
+        return z
+
+    def backprop(self, dz):
+        dx = dz
+        dy = dz
+        return dx, dy
+
+
+a = 2
+b = 3
+c = 4
+add = Add()
+mul = Multiply()
+
+print("順伝播出力:"+str(mul.forward(add.forward(a, b), c)))
+print("逆伝播出力 da:"+str(add.backprop(mul.forward(add.forward(a, b), c))))
+
